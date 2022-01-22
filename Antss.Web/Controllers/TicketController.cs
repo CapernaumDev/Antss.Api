f3af47671fe9c81c072116a77d4553d74143eb35
Will Lewis Williams <will.lewis.w@gmail.com>
2022-01-22 16:10:55 +0000

    ticket controller

---
+++ Antss.Web/Controllers/TicketController.cs
@@ -0,0 +1,26 @@
+﻿using Antss.Data;
+using Antss.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Antss.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly AntssContext _db;
+
+        public TicketController(AntssContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        [Route("List")]
+        public async Task<ActionResult<IEnumerable<Ticket>>> Get()
+        {
+            return await _db.Tickets.Include(x => x.RaisedBy).ToListAsync();
+        }
+    }
+}
