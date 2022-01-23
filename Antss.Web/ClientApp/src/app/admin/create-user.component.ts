6f6757c91b6ffa5088dc70e336b4e9985f6cdcba
David <davidsutcliffe@mail.com>
2022-01-23 14:28:03 +0000

    Add create user form front end

---
+++ Antss.Web/ClientApp/src/app/admin/create-user.component.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from '../api.service';
+import { User } from '../models/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'create-user',
+  templateUrl: './create-user.component.html'
+})
+
+export class CreateUserComponent implements OnInit {
+  submitted = false;
+  registerForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+    //True if all the fields are filled
+    if (this.submitted) {
+      alert("Great!!");
+    }
+
+  }
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      emailAddress: ['', [Validators.required, Validators.email]],
+      userType: ['', [Validators.required]],
+      location: ['', [Validators.required]],
+      contactNumber: ['', [Validators.required]]
+    });
+  }
+}
+
--- Antss.Web/ClientApp/src/app/app.module.ts
+++ Antss.Web/ClientApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -8,6 +9,7 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { UserListComponent } from './admin/user-list.component';
+import { CreateUserComponent } from './admin/create-user.component';
 import { TicketListComponent } from './tickets/ticket-list.component';
 
 @NgModule({
@@ -16,16 +18,19 @@ import { TicketListComponent } from './tickets/ticket-list.component';
     NavMenuComponent,
     HomeComponent,
     UserListComponent,
+    CreateUserComponent,
     TicketListComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'admin', component: UserListComponent },
-      { path: 'tickets', component: TicketListComponent}
+      { path: 'user-list', component: UserListComponent },
+      { path: 'create-user', component: CreateUserComponent },
+      { path: 'ticket-list', component: TicketListComponent }
     ])
   ],
   providers: [],
