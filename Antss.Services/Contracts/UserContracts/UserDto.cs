c60999a459fa663525ad7577cc09bfaa77c93cd6
David <davidsutcliffe@mail.com>
2022-01-27 15:59:53 +0000

    make one-way property nullable

--- Antss.Services/Contracts/UserContracts/UserDto.cs
+++ Antss.Services/Contracts/UserContracts/UserDto.cs
@@ -9,6 +9,6 @@
         public string ContactNumber { get; set; }
         public string EmailAddress { get; set; }
         public int UserTypeId { get; set; }
-        public string UserType { get; set; }
+        public string? UserType { get; set; }
     }
 }
