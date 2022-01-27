86b5cb45c5d75bee7769245be29984b2676b1e8a
David <davidsutcliffe@mail.com>
2022-01-27 21:24:16 +0000

    add a fade effect to hompage welcome message area

--- Antss.Web/ClientApp/src/app/public/home.component.ts
+++ Antss.Web/ClientApp/src/app/public/home.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { AppStoreService } from "@core/app.store.service";
 import { Subscription } from 'rxjs';
+import {trigger, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  animations: [
+    trigger('fade', [ 
+      transition('void => *', [
+        style({ opacity: 0 }), 
+        animate(500, style({opacity: 1}))
+      ]) 
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   public isSigningIn!: boolean;
--- Antss.Web/ClientApp/src/app/public/public.module.ts
+++ Antss.Web/ClientApp/src/app/public/public.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LoginComponent } from './login.component';
 import { HomeComponent } from './home.component';
@@ -16,6 +17,7 @@ import { HomeComponent } from './home.component';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'login', component: LoginComponent }
