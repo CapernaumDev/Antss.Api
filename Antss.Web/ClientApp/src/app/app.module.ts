87cdf8b87c84130d7877adc84ea0126d9bf5a42a
David <davidsutcliffe@mail.com>
2022-02-06 15:11:14 +0000

    cache routes

--- Antss.Web/ClientApp/src/app/app.module.ts
+++ Antss.Web/ClientApp/src/app/app.module.ts
@@ -8,13 +8,14 @@ import { AppStartup } from './core/app.startup';
 import { CoreModule } from './core/core.module';
 import { PublicModule } from './public/public.module';
 import { PrivateModule } from './private/private.module';
-import { RouterModule } from '@angular/router';
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { Effects } from '@core/store/effects';
 import { Reducers } from '@core/store/reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { CacheRouteReuseStrategy } from './core/cache-route-reuse-strategy ';
 
 @NgModule({
   declarations: [
@@ -38,7 +39,11 @@ import { environment } from '../environments/environment';
   providers: [
     { provide: APP_INITIALIZER, multi: true, deps: [AppStartup], useFactory: (startupClass: AppStartup) => () => startupClass.attemptTokenLogin() },
     { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { 
+      provide: RouteReuseStrategy,
+      useClass: CacheRouteReuseStrategy
+    }
   ],
   bootstrap: [AppComponent]
 })
---
+++ Antss.Web/ClientApp/src/app/core/cache-route-reuse-strategy .ts
@@ -0,0 +1,34 @@
+import { RouteReuseStrategy } from '@angular/router/';
+import { ActivatedRouteSnapshot, DetachedRouteHandle } from '@angular/router';
+
+export class CacheRouteReuseStrategy implements RouteReuseStrategy {
+    storedRouteHandles = new Map<string, DetachedRouteHandle>();
+
+    shouldReuseRoute(before: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+        return before.routeConfig === curr.routeConfig;
+    }
+
+    retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle | null {
+        return this.storedRouteHandles.get(this.getPath(route)) as DetachedRouteHandle;
+    }
+
+    shouldAttach(route: ActivatedRouteSnapshot): boolean {
+        return this.storedRouteHandles.has(this.getPath(route));
+    }
+
+    shouldDetach(route: ActivatedRouteSnapshot): boolean {
+        return true;
+    }
+
+    store(route: ActivatedRouteSnapshot, detachedTree: DetachedRouteHandle): void {
+        this.storedRouteHandles.set(this.getPath(route), detachedTree);
+    }
+
+    private getPath(route: ActivatedRouteSnapshot): string {
+        if (route.routeConfig !== null && route.routeConfig.path !== null) {
+            return route.routeConfig.path || '';
+      }
+
+        return '';
+    }
+}
